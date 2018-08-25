8b28194d59af88011374462797f757233d2443e8
Agieaja <tiar@edutren.com>
2018-08-25 17:40:40 +0700

    function for load view list nilai by murid

--- application/controllers/nilai_murid.php
+++ application/controllers/nilai_murid.php
@@ -40,5 +40,16 @@ class nilai_murid extends CI_Controller {
 
         $this->load->view('v_home', $data);
     }
+
+    function list_nilai_murid()
+    {
+        $data['header'] = "header/v_header";
+        $data['navbar'] = "navbar/v_navbar";
+        $data['sidebar'] = "sidebar/v_sidebar";
+        $data['footer'] = "footer/v_footer";
+        $data['body'] = "nilai_murid/v_list_nilai_murid";
+
+        $this->load->view('v_home', $data);
+    }
     
 }
