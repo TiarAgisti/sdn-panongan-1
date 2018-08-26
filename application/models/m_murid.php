e8f05e4c0c12a80ed25a25147a9ae62056ecf361
Agieaja <tiar@edutren.com>
2018-08-26 20:05:56 +0700

    add where m.status

--- application/models/m_murid.php
+++ application/models/m_murid.php
@@ -9,7 +9,8 @@ class m_murid extends CI_Model{
 		$query = $this->db->query('SELECT kode_murid,nisn,nama_murid,tahun_ajaran
         ,concat(kls.tingkat_kelas,kls.keterangan_tingkat) as ket_kelas 
         FROM murid as m
-        inner join kelas as kls on kls.kode_kelas = m.kode_kelas');
+        inner join kelas as kls on kls.kode_kelas = m.kode_kelas
+        where m.status = 1');
 		return $query->result();
 	}	
 }
