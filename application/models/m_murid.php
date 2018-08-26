69d9041d88496b0902de58fa77b3a86baf8ede78
Agieaja <tiar@edutren.com>
2018-08-26 20:17:30 +0700

    update query

--- application/models/m_murid.php
+++ application/models/m_murid.php
@@ -6,7 +6,7 @@ class m_murid extends CI_Model{
     }
 
     function listmurid(){
-		$query = $this->db->query('SELECT kode_murid,nisn,nama_murid,tahun_ajaran
+		$query = $this->db->query('SELECT m.kode_murid,m.nisn,m.nama_murid,m.tahun_ajaran
         ,concat(kls.tingkat_kelas,kls.keterangan_tingkat) as ket_kelas 
         FROM murid as m
         inner join kelas as kls on kls.kode_kelas = m.kode_kelas
