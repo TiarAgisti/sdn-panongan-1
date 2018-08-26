e2e1450c8b853168f93a91b9e89b39da047ea88b
Agieaja <tiar@edutren.com>
2018-08-26 22:09:59 +0700

    function for get list kenaikan kelas

--- application/models/m_kenaikan_kelas.php
+++ application/models/m_kenaikan_kelas.php
@@ -3,4 +3,10 @@ class m_kenaikan_kelas extends CI_Model{
     function __construct(){
         parent::__construct();
     }
+
+    function retrievelist(){
+        $query = $this->db->query('SELECT tahun_ajaran,dari_kode_kelas,ke_kode_kelas 
+        FROM kenaikan_kelas WHERE status = 1');
+        return $query->result();
+    }
 }
