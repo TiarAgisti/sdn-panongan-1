6c0793c30f16c33e9d200ad14347dc73f7c4c540
Agieaja <tiar@edutren.com>
2018-08-26 21:58:50 +0700

    function for get data users

--- application/models/m_login.php
+++ application/models/m_login.php
@@ -3,5 +3,10 @@
 class m_login extends CI_Model{	
 	function cek_login($table,$where){		
 		return $this->db->get_where($table,$where);
-	}	
+	}
+	
+	function retrieveuser(){
+		$query = $this->db->query('SELECT kode_user,nama_user,tipe_user FROM users where status = 1');
+		return $query->result();
+	}
 }
