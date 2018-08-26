cbb6faabe8ca50fc99f38071fb46e04f29a1b44e
Agieaja <tiar@edutren.com>
2018-08-27 05:55:22 +0700

    function generate kode mapel

--- application/models/m_mapel.php
+++ application/models/m_mapel.php
@@ -8,5 +8,17 @@ class m_mapel extends CI_Model{
 	function listmapel(){
 		$query = $this->db->query('SELECT * FROM mata_pelajaran where status = 1');
 		return $query->result();
-	}	
+	}
+	
+	function getKodeMapel(){
+		$query = $this->db->query("SELECT max(kode_mapel) as max_code FROM mata_pelajaran");
+		$row = $query->row_array();
+
+		$max_id = $row['max_code'];
+		$max_fix = (int) substr($max_id,1,5);
+
+		$max_mapel = $max_fix + 1;
+		$kode_mapel = "M".sprintf("%05s", $max_mapel);
+		return $kode_mapel;
+	}
 }
