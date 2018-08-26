b880533ed1b767c6c1997d87aa0fe14d87c0895c
Agieaja <tiar@edutren.com>
2018-08-26 20:01:57 +0700

    change query get data murid

--- application/models/m_murid.php
+++ application/models/m_murid.php
@@ -6,7 +6,10 @@ class m_murid extends CI_Model{
     }
 
     function listmurid(){
-		$query = $this->db->query('SELECT * FROM murid where status = 1');
+		$query = $this->db->query('SELECT kode_murid,nisn,nama_murid,tahun_ajaran
+        ,concat(kls.tingkat_kelas,kls.keterangan_tingkat) as ket_kelas 
+        FROM murid as m
+        inner join kelas as kls on kls.kode_kelas = m.kode_kelas');
 		return $query->result();
 	}	
 }
