49100bc370eea4f7eddf6f1a9f6a1ccd0fc4f6a7
Agieaja <tiar@edutren.com>
2018-08-26 22:17:35 +0700

    update query

--- application/models/m_kenaikan_kelas.php
+++ application/models/m_kenaikan_kelas.php
@@ -5,8 +5,13 @@ class m_kenaikan_kelas extends CI_Model{
     }
 
     function retrievelist(){
-        $query = $this->db->query('SELECT tahun_ajaran,dari_kode_kelas,ke_kode_kelas 
-        FROM kenaikan_kelas WHERE status = 1');
+        $query = $this->db->query('SELECT kk.tahun_ajaran
+        ,kls1.tingkat_kelas as dari_kelas
+        ,kls2.tingkat_kelas as ke_kelas
+        FROM kenaikan_kelas as kk
+        INNER JOIN kelas as kls1 ON kls1.kode_kelas = kk.dari_kode_kelas
+        INNER JOIN kelas as kls2 ON kls2.kode_kelas = kk.ke_kode_kelas
+        WHERE kk.status = 1');
         return $query->result();
     }
 }
