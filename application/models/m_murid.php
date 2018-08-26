2658c3e8e0acc0bf9dc7e62f27ce58ec331e070c
Agieaja <tiar@edutren.com>
2018-08-26 08:38:08 +0700

    create m_murid,create function for get data murid

---
+++ application/models/m_murid.php
@@ -0,0 +1,13 @@
+<?php
+
+class m_murid extends CI_Model{
+    function __construct(){
+        parent::__construct();
+    }
+
+    function listmurid()
+    {
+        $query = $this->db-query('SELECT * FROM murid where status = 1');
+        return $query->result();
+    }
+}
