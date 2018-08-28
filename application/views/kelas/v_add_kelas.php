a9a7aa0fe2de6339a99a2cddf77749345e5447c6
Agieaja <tiar@edutren.com>
2018-08-29 06:27:35 +0700

    add form action

--- application/views/kelas/v_add_kelas.php
+++ application/views/kelas/v_add_kelas.php
@@ -1,16 +1,18 @@
-<div class="panel panel-default">
-	<div class="panel-heading">Tambah Data Kelas</div>
-	<div class="panel-body">
-        <label>Kode Kelas:</label>
-        <input type="text" class="form-control" name="kd_kls" placeholder="Generate Otomatis" readonly required>
-        <label>Tingkat Kelas:</label>
-        <input type="number" class="form-control" name="tngkt_kls" min="1" max="6" required>
-        <label>Keterangan Tingkat:</label>
-        <input type="text" class="form-control" name="ket_tngkt_kls" maxlength="2" required>
-        <br/>
-        <button type="submit" class="btn btn-primary">Simpan</button>
-        <a href="<?php echo base_url();?>kelas"  class="btn btn-danger">
-            Batal
-        </a>
-	</div>
-</div>
+<form method="post" action="<?php echo base_url();?>kelas/simpan">
+    <div class="panel panel-default">
+        <div class="panel-heading">Tambah Data Kelas</div>
+        <div class="panel-body">
+            <label>Kode Kelas:</label>
+            <input type="text" class="form-control" name="kd_kls" placeholder="Generate Otomatis" readonly required>
+            <label>Tingkat Kelas:</label>
+            <input type="number" class="form-control" name="tngkt_kls" min="1" max="6" required>
+            <label>Keterangan Tingkat:</label>
+            <input type="text" class="form-control" name="ket_tngkt_kls" maxlength="2" required>
+            <br/>
+            <button type="submit" class="btn btn-primary">Simpan</button>
+            <a href="<?php echo base_url();?>kelas"  class="btn btn-danger">
+                Batal
+            </a>
+        </div>
+    </div>
+</form>
--- application/views/kelas/v_edit_kelas.php
+++ application/views/kelas/v_edit_kelas.php
@@ -1,16 +1,18 @@
-<div class="panel panel-default">
-	<div class="panel-heading">Edit Data Kelas</div>
-	<div class="panel-body">
-        <label>Kode Kelas:</label>
-        <input type="text" class="form-control" name="kd_kls" readonly required>
-        <label>Tingkat Kelas:</label>
-        <input type="number" class="form-control" name="tngkt_kls" min="1" max="6" required>
-        <label>Keterangan Tingkat:</label>
-        <input type="text" class="form-control" name="ket_tngkt_kls" maxlength="2" required>
-        <br/>
-        <button type="submit" class="btn btn-primary">Simpan</button>
-        <a href="<?php echo base_url();?>kelas"  class="btn btn-danger">
-            Batal
-        </a>
-	</div>
-</div>
+<form method="post" action="<?php echo base_url();?>kelas/ubah">
+    <div class="panel panel-default">
+        <div class="panel-heading">Edit Data Kelas</div>
+        <div class="panel-body">
+            <label>Kode Kelas:</label>
+            <input type="text" class="form-control" name="kd_kls" value="<?php echo $kode_kelas;?>" readonly required>
+            <label>Tingkat Kelas:</label>
+            <input type="number" class="form-control" name="tngkt_kls" min="1" max="6" value="<?php echo $tingkat_kelas;?>" required>
+            <label>Keterangan Tingkat:</label>
+            <input type="text" class="form-control" name="ket_tngkt_kls" maxlength="2" value="<?php echo $keterangan_tingkat;?>" required>
+            <br/>
+            <button type="submit" class="btn btn-primary">Simpan</button>
+            <a href="<?php echo base_url();?>kelas"  class="btn btn-danger">
+                Batal
+            </a>
+        </div>
+    </div>
+</form>
