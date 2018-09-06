7ba531d8aae82112e5a2e07dbd8902c574e0e577
Agieaja <tiar@edutren.com>
2018-09-06 23:05:41 +0700

    for generate kode_kelas

--- application/models/m_kelas.php
+++ application/models/m_kelas.php
@@ -8,4 +8,16 @@ class m_kelas extends CI_Model{
         $query = $this->db->query('SELECT * FROM kelas where status = 1');
         return $query->result();
     }
+
+    function getKodeKelas(){
+		$query = $this->db->query("SELECT max(kode_kelas) as max_code FROM kelas");
+		$row = $query->row_array();
+
+		$max_id = $row['max_code'];
+		$max_fix = (int) substr($max_id,1,5);
+
+		$max_kelas = $max_fix + 1;
+		$kode_kelas = "K".sprintf("%05s", $max_kelas);
+		return $kode_kelas;
+	}
 }
