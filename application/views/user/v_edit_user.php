1852e372e6a7369116c58b541c1bbf4168863eef
Agieaja <tiar@edutren.com>
2018-09-07 00:02:50 +0700

    change view user

--- application/views/user/v_edit_user.php
+++ application/views/user/v_edit_user.php
@@ -1,22 +1,19 @@
 <div class="panel panel-default">
 	<div class="panel-heading">Edit Data User</div>
 	<div class="panel-body">
+        <label>Kode User:</label>
+        <input type="text" class="form-control" name="kd_usr" readonly>
+        <label>Nama User:</label>
+        <input type="text" class="form-control" name="nm_usr" maxlength="50">
+        <label>Password User:</label>
+        <input type="password" class="form-control" name="pass_usr" maxlength="20">
         <label>Tipe User:</label>
         <select name="tipe_usr" class="form-control">
             <option value="">--PILIH--</option>
             <option value="Guru">Guru</option>
-            <option value="Murid">Murid</option>
+            <option value="Orangtua">Orangtua</option>
+            <option value="Admin">Admin</option>
         </select>
-        <label>Kode User:</label>
-        <select name="kd_usr" class="form-control">
-            <option value="">--PILIH--</option>
-            <option value="G00001">G00001</option>
-            <option value="G00002">G00002</option>
-        </select>
-        <label>Nama User:</label>
-        <input type="text" class="form-control" name="nm_usr" readonly>
-        <label>Password User:</label>
-        <input type="password" class="form-control" name="pass_usr">
         <br/>
         <button type="submit" class="btn btn-primary">Simpan</button>
         <a href="<?php echo base_url();?>user"  class="btn btn-danger">
