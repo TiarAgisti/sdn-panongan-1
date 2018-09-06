75d7d4ad0071441d2b3483ad3ed1ecf1e64278a2
Agieaja <tiar@edutren.com>
2018-09-07 00:04:08 +0700

    generate kode_user

--- application/models/m_login.php
+++ application/models/m_login.php
@@ -9,4 +9,16 @@ class m_login extends CI_Model{
 		$query = $this->db->query('SELECT kode_user,nama_user,tipe_user FROM users where status = 1');
 		return $query->result();
 	}
+
+	function getKodeUser(){
+		$query = $this->db->query("SELECT max(kode_user) as max_code FROM users");
+		$row = $query->row_array();
+
+		$max_id = $row['max_code'];
+		$max_fix = (int) substr($max_id,1,5);
+
+		$max_user = $max_fix + 1;
+		$kode_user = "U".sprintf("%05s", $max_user);
+		return $kode_user;
+	}	
 }
