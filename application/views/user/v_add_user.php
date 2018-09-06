629c67f2853c594fb020971be3477f93db9c817a
Agieaja <tiar@edutren.com>
2018-09-07 00:15:50 +0700

    add form action

--- application/views/user/v_add_user.php
+++ application/views/user/v_add_user.php
@@ -1,23 +1,25 @@
-<div class="panel panel-default">
-	<div class="panel-heading">Tambah Data User</div>
-	<div class="panel-body">
-        <label>Kode User:</label>
-        <input type="text" class="form-control" name="kd_usr" readonly>
-        <label>Nama User:</label>
-        <input type="text" class="form-control" name="nm_usr" maxlength="50">
-        <label>Password User:</label>
-        <input type="password" class="form-control" name="pass_usr" maxlength="20">
-        <label>Tipe User:</label>
-        <select name="tipe_usr" class="form-control">
-            <option value="">--PILIH--</option>
-            <option value="Guru">Guru</option>
-            <option value="Orangtua">Orangtua</option>
-            <option value="Admin">Admin</option>
-        </select>
-        <br/>
-        <button type="submit" class="btn btn-primary">Simpan</button>
-        <a href="<?php echo base_url();?>user"  class="btn btn-danger">
-            Batal
-        </a>
-	</div>
-</div>
+<form method="post" action="<?php echo base_url();?>user/simpan">
+    <div class="panel panel-default">
+        <div class="panel-heading">Tambah Data User</div>
+        <div class="panel-body">
+            <label>Kode User:</label>
+            <input type="text" class="form-control" name="kd_usr" readonly>
+            <label>Nama User:</label>
+            <input type="text" class="form-control" name="nm_usr" maxlength="50">
+            <label>Password User:</label>
+            <input type="password" class="form-control" name="pass_usr" maxlength="20">
+            <label>Tipe User:</label>
+            <select name="tipe_usr" class="form-control">
+                <option value="">--PILIH--</option>
+                <option value="Guru">Guru</option>
+                <option value="Orangtua">Orangtua</option>
+                <option value="Admin">Admin</option>
+            </select>
+            <br/>
+            <button type="submit" class="btn btn-primary">Simpan</button>
+            <a href="<?php echo base_url();?>user"  class="btn btn-danger">
+                Batal
+            </a>
+        </div>
+    </div>
+</form>
--- application/views/user/v_edit_user.php
+++ application/views/user/v_edit_user.php
@@ -1,23 +1,25 @@
-<div class="panel panel-default">
-	<div class="panel-heading">Edit Data User</div>
-	<div class="panel-body">
-        <label>Kode User:</label>
-        <input type="text" class="form-control" name="kd_usr" readonly>
-        <label>Nama User:</label>
-        <input type="text" class="form-control" name="nm_usr" maxlength="50">
-        <label>Password User:</label>
-        <input type="password" class="form-control" name="pass_usr" maxlength="20">
-        <label>Tipe User:</label>
-        <select name="tipe_usr" class="form-control">
-            <option value="">--PILIH--</option>
-            <option value="Guru">Guru</option>
-            <option value="Orangtua">Orangtua</option>
-            <option value="Admin">Admin</option>
-        </select>
-        <br/>
-        <button type="submit" class="btn btn-primary">Simpan</button>
-        <a href="<?php echo base_url();?>user"  class="btn btn-danger">
-            Batal
-        </a>
-	</div>
-</div>
+<form method="post" action="<?php echo base_url();?>user/ubah">
+    <div class="panel panel-default">
+        <div class="panel-heading">Edit Data User</div>
+        <div class="panel-body">
+            <label>Kode User:</label>
+            <input type="text" class="form-control" name="kd_usr" value="<?php echo $kode_user;?>" readonly>
+            <label>Nama User:</label>
+            <input type="text" class="form-control" name="nm_usr" maxlength="50" value="<?php echo $nama_user;?>" required>
+            <label>Password User:</label>
+            <input type="password" class="form-control" name="pass_usr" maxlength="20" value="<?php echo $password_user;?>" required>
+            <label>Tipe User:</label>
+            <select name="tipe_usr" class="form-control">
+                <option value="<?php echo $tipe_user;?>" selected><?php echo $tipe_user;?></option>
+                <option value="Guru">Guru</option>
+                <option value="Orangtua">Orangtua</option>
+                <option value="Admin">Admin</option>
+            </select>
+            <br/>
+            <button type="submit" class="btn btn-primary">Simpan</button>
+            <a href="<?php echo base_url();?>user"  class="btn btn-danger">
+                Batal
+            </a>
+        </div>
+    </div>
+</form>
