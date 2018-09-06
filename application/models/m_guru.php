79b8ae20c4b46164cc286df5d0996f2038678a6c
Agieaja <tiar@edutren.com>
2018-09-06 23:31:20 +0700

    generate kode_guru

--- application/models/m_guru.php
+++ application/models/m_guru.php
@@ -8,5 +8,17 @@ class m_guru extends CI_Model{
 	function listguru(){
 		$query = $this->db->query('SELECT * FROM guru where status = 1');
 		return $query->result();
+	}
+
+	function getKodeGuru(){
+		$query = $this->db->query("SELECT max(kode_guru) as max_code FROM guru");
+		$row = $query->row_array();
+
+		$max_id = $row['max_code'];
+		$max_fix = (int) substr($max_id,1,5);
+
+		$max_guru = $max_fix + 1;
+		$kode_guru = "G".sprintf("%05s", $max_guru);
+		return $kode_guru;
 	}	
 }
