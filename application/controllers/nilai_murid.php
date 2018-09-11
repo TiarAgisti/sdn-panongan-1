3921e265cac31232baf9bf8cbc94577c1f4e867c
Agieaja <tiar@edutren.com>
2018-09-11 10:06:22 +0700

    edit nilai murid

--- application/controllers/nilai_murid.php
+++ application/controllers/nilai_murid.php
@@ -45,7 +45,8 @@ class nilai_murid extends CI_Controller {
         $sql_murid = "SELECT m.kode_murid,m.nisn,m.nama_murid,m.tahun_ajaran,m.kode_kelas
         ,concat(kls.tingkat_kelas,kls.keterangan_tingkat) as ket_kelas 
         FROM murid as m inner join kelas as kls on kls.kode_kelas = m.kode_kelas where m.status = 1 and m.kode_murid = '$kodeMurid'";
-        $sql_mapel = "select kode_mapel,nama_mapel from mata_pelajaran where status = 1";
+        $sql_mapel = "select kode_mapel,nama_mapel from mata_pelajaran where status = 1 and kode_mapel not in(select kode_mapel
+        from nilai_murid where kode_murid = '$kodeMurid')";
 
         $res = $this->db->query($sql_murid)->row();
         $data['kd_murid'] = $res->kode_murid;
@@ -97,26 +98,68 @@ class nilai_murid extends CI_Controller {
         $kodeMurid = $this->uri->segment(3);
         $kodeMapel = $this->uri->segment(4);
 
-        $sql_nilai = "select nm.kode_murid,nm.kode_mapel,nm.kode_kelas,nm.tahun_ajaran,nm.nilai
+        $sqlNilai = "select nm.kode_murid,nm.kode_mapel,nm.kode_kelas,nm.tahun_ajaran,nm.nilai
         ,m.nama_murid,m.nisn,mapel.nama_mapel,concat(kls.tingkat_kelas,kls.keterangan_tingkat) as ket_kelas from nilai_murid as nm
         inner join murid as m on m.kode_murid = nm.kode_murid
         inner join kelas as kls on kls.kode_kelas = nm.kode_kelas
         inner join mata_pelajaran as mapel on mapel.kode_mapel = nm.kode_mapel
         where nm.kode_murid = '$kodeMurid' and nm.kode_mapel = '$kodeMapel'";
 
-        $res = $this->db->query($sql_nilai)->row();
-        $data['kd_murid'] = $res->kode_murid;
-        $data['nisn_murid'] = $res->nisn;
-        $data['nm_murid'] = $res->nama_murid;
-        $data['thn_ajaran'] = $res->tahun_ajaran;
-        $data['kd_mapel'] = $res->kode_mapel;
-        $data['nm_mapel'] = $res->nama_mapel;
+        $res = $this->db->query($sqlNilai)->row();
+        $data['kdMurid'] = $res->kode_murid;
+        $data['nisnMurid'] = $res->nisn;
+        $data['nmMurid'] = $res->nama_murid;
+        $data['thnAjaran'] = $res->tahun_ajaran;
+        $data['kdMapel'] = $res->kode_mapel;
+        $data['nmMapel'] = $res->nama_mapel;
         $data['nilai'] = $res->nilai;
-        $data['kd_kelas'] = $res->kode_kelas;
-        $data['kelas'] = $res->ket_kelas;
+        $data['kdKelas'] = $res->kode_kelas;
+        $data['ketKelas'] = $res->ket_kelas;
 
 
         $this->load->view('v_home', $data);
+    }
+
+    function simpan()
+    {
+        $kd_murid = trim($this->input->post('kd_murid'));
+        $kd_kls = trim($this->input->post('kd_kls'));
+        $kd_mapel = trim($this->input->post('mapel_murid'));
+        $thn_ajaran = trim($this->input->post('thn_ajaran'));
+        $nilai = trim($this->input->post('nilai_murid'));
+        $kode_user=$this->session->userdata('kode_user');
+        $tanggal = date('Y-m-d');
+
+        $data['kode_murid'] = $kd_murid;
+        $data['kode_kelas'] = $kd_kls;
+        $data['kode_mapel'] = $kd_mapel;
+        $data['tahun_ajaran'] = $thn_ajaran;
+        $data['nilai'] = $nilai;
+        $data['created_date'] = $tanggal;
+        $data['created_by'] = $kode_user;
+        $data['updated_date'] = $tanggal;
+        $data['updated_by'] = $kode_user;
+        $data['status'] = 1;
+
+        $this->db->trans_start();
+        $this->db->insert('nilai_murid', $data);
+        $this->db->trans_complete();
+        if ($this->db->trans_status() === FALSE)
+        {
+            $this->session->set_flashdata("msg", "<div class='alert alert-danger' role='alert'>
+                <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+                <strong>Peringatan!</strong> Data gagal tersimpan.
+                </div>");
+            redirect('nilai_murid'); 
+        }
+        else 
+        {
+            $this->session->set_flashdata("msg", "<div class='alert alert-info' role='alert'>
+            <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+            <strong>Informasi!</strong> Data berhasil tersimpan. 
+            </div>");
+            redirect('nilai_murid'); 
+        }
     }
     
 }
--- application/views/nilai_murid/v_edit_nilai.php
+++ application/views/nilai_murid/v_edit_nilai.php
@@ -1,19 +1,19 @@
 <div class="panel panel-default">
 	<div class="panel-heading">Edit Nilai Murid</div>
 	<div class="panel-body">
-        <input type="text" class="form-control" name="kd_murid" value="<?php echo $kd_murid;?>" readonly hidden>
+        <input type="text" class="form-control" name="kd_murid" value="<?php echo $kdMurid;?>" readonly hidden>
         <label>NISN:</label>
-        <input type="text" class="form-control" name="nisn_murid" value="<?php echo $nisn_murid;?>" readonly required>
+        <input type="text" class="form-control" name="nisn_murid" value="<?php echo $nisnMurid;?>" readonly required>
         <label>Nama:</label>
-        <input type="text" class="form-control" name="nm_murid" value="<?php echo $nm_murid;?>" readonly required>
-        <input type="text" class="form-control" name="kd_kls" value="<?php echo $kd_kelas;?>" readonly hidden>
+        <input type="text" class="form-control" name="nm_murid" value="<?php echo $nmMurid;?>" readonly required>
+        <input type="text" class="form-control" name="kd_kls" value="<?php echo $kdKelas;?>" readonly hidden>
         <label>Kelas:</label>
-        <input type="text" class="form-control" name="kls_murid" value="<?php echo $kelas;?>" readonly required>
+        <input type="text" class="form-control" name="kls_murid" value="<?php echo $ketKelas;?>" readonly required>
         <label>Tahun Ajaran:</label>
-        <input type="number" class="form-control" name="thn_ajaran" value="<?php echo $thn_ajaran;?>" readonly required>
-        <input type="text" class="form-control" name="kd_mapel" value="<?php echo $kd_mapel;?>" readonly hidden>
+        <input type="number" class="form-control" name="thn_ajaran" value="<?php echo $thnAjaran;?>" readonly required>
+        <input type="text" class="form-control" name="kd_mapel" value="<?php echo $kdMapel;?>" readonly hidden>
         <label>Mata Pelajaran:</label>
-        <input type="text" class="form-control" name="mapel_murid" value="<?php echo $nm_mapel;?>" readonly required>
+        <input type="text" class="form-control" name="mapel_murid" value="<?php echo $nmMapel;?>" readonly required>
         <label>Nilai:</label>
         <input type="number" class="form-control" name="nilai_murid" value="<?php echo $nilai;?>" min="0" max="100" required>
         <br/>
