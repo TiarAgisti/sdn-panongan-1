5338e24118d41b13bc702232b0a2157ea250ec43
Agieaja <tiar@edutren.com>
2018-09-16 22:07:15 +0700

    create function retrieve murid by kelas

--- application/models/m_raport_murid.php
+++ application/models/m_raport_murid.php
@@ -9,4 +9,9 @@ class m_raport_murid extends CI_Model{
 		$query = $this->db->query('SELECT kode_kelas,concat(tingkat_kelas,keterangan_tingkat) as ket_kelas FROM kelas where status = 1');
 		return $query->result();
 	}
+
+	function retrieveMuridByKelas($kodeKelas){
+		$query = $this->db->query('SELECT kode_murid,nisn,nama_murid,tahun_ajaran FROM murid where status = 1 and kode_kelas = "$kodeKelas"');
+		return $query->result();	
+	}
 }
