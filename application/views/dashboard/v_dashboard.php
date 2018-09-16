886f5c3e3683b076e99fd10e83f305045334fe4f
Agieaja <tiar@edutren.com>
2018-09-16 09:50:37 +0700

    update dashboard

--- application/views/dashboard/v_dashboard.php
+++ application/views/dashboard/v_dashboard.php
@@ -1,4 +1,75 @@
- <div class="panel panel-primary">
- 	<div class="panel-heading">Selamat Data , <?php echo $this->session->userdata('nama');?></div>
- 	<div class="panel-body">SDN PANONGAN I</div>
- </div>
+<div class="row purchace-popup">
+  <div class="col-12">
+    <span class="d-flex alifn-items-center">
+      <p>Welcome to SDN Panongan 1</p>
+    </span>
+  </div>
+</div>
+<div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 grid-margin stretch-card">
+  <div class="card card-statistics">
+    <div class="card-body">
+      <div class="clearfix">
+		    <div class="float-left">
+    			<i class="mdi mdi-account-location text-info icon-lg"></i>
+    		</div>
+      	<div class="float-right">
+    		  <p class="mb-0 text-right">Guru</p>
+    			<div class="fluid-container">
+        		<h3 class="font-weight-medium text-right mb-0">24</h3>
+      		</div>
+    		</div>
+    	</div>
+  	</div>
+	</div>
+</div>
+<div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 grid-margin stretch-card">
+  <div class="card card-statistics">
+    <div class="card-body">
+      <div class="clearfix">
+        <div class="float-left">
+          <i class="mdi mdi-account-location text-info icon-lg"></i>
+        </div>
+        <div class="float-right">
+          <p class="mb-0 text-right">Murid</p>
+          <div class="fluid-container">
+            <h3 class="font-weight-medium text-right mb-0">246</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 grid-margin stretch-card">
+  <div class="card card-statistics">
+    <div class="card-body">
+      <div class="clearfix">
+        <div class="float-left">
+          <i class="mdi mdi-receipt text-warning icon-lg"></i>
+        </div>
+        <div class="float-right">
+          <p class="mb-0 text-right">Mata Pelajaran</p>
+          <div class="fluid-container">
+            <h3 class="font-weight-medium text-right mb-0">34</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 grid-margin stretch-card">
+  <div class="card card-statistics">
+    <div class="card-body">
+      <div class="clearfix">
+        <div class="float-left">
+          <i class="mdi mdi-receipt text-warning icon-lg"></i>
+        </div>
+        <div class="float-right">
+          <p class="mb-0 text-right">Kelas</p>
+          <div class="fluid-container">
+            <h3 class="font-weight-medium text-right mb-0">14</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
