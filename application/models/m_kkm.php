60e7edba656462c4434c1a4849a01e1a07b6f075
Agieaja <tiar@edutren.com>
2018-09-16 10:11:34 +0700

    model for kkm

---
+++ application/models/m_kkm.php
@@ -0,0 +1,21 @@
+<?php
+
+class m_kkm extends CI_Model{	
+	function retrievekkm(){
+		$query = $this->db->query('SELECT kkm.kode_kkm,kkm.tingkat_kelas,kkm.nilai_kkm,mapel.nama_mapel FROM kkm_murid as kkm 
+		inner join mata_pelajaran as mapel on mapel.kode_mapel = kkm.kode_mapel where status = 1');
+		return $query->result();
+	}
+
+	function getKodeKKM(){
+		$query = $this->db->query("SELECT max(kode_kkm) as max_code FROM kkm_murid");
+		$row = $query->row_array();
+
+		$max_id = $row['max_code'];
+		$max_fix = (int) substr($max_id,1,5);
+
+		$max_kkm = $max_fix + 1;
+		$kode_kkm = "N".sprintf("%05s", $max_kkm);
+		return $kode_kkm;
+	}	
+}
